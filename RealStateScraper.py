9e456cfeec73c051112a7b0d337c12952592fbd6
kevin <castillokevin37@gmail.com>
2020-07-08 00:56:11 -0500

    First data frame

--- RealStateScraper.py
+++ RealStateScraper.py
@@ -11,13 +11,34 @@ def Peticion(url):
     ContenedorGeneral = Soup.find_all("div",class_="posting-card super-highlighted")
     
     NumeroImagenes = Soup.find_all("span",class_="ftag-text photoNumber")
-    TituloCabezara = Soup.find_all("h2", class_="posting-title")
+    TituloCabecera = Soup.find_all("h2", class_="posting-title")
     DireccionCabezara = Soup.find_all("span",class_="posting-location")
     ListaCaracteristicas = Soup.find_all("ul",class_="main-features")
     Costo = Soup.find_all("span",class_="first-price")
     Descripcion = Soup.find_all("div", class_="posting-description")
     FechaVenta = Soup.find_all("ul",class_="posting-features")
     
+    ListaNumeroImagen = []
+    ListaTituloCabecera = []
+    ListaDireccionCabecera = []
+    ListaCosto = []
+    ListaFechaVenta = []
 
+    for i in ContenedorGeneral:
+
+        NumeroIm = i.find('span',class_ = "ftag-text photoNumber")
+        ListaNumeroImagen.append(NumeroIm.text)
+
+        TituloCab = i.find('h2',class_="posting-title")
+        ListaTituloCabecera.append(TituloCab.text)
+        
+        DireccionCab = i.find("span",class_="posting-location")
+        ListaDireccionCabecera.append(DireccionCab)
+        
+        CostoAux = i.find("span",class_="first-price")
+        ListaCosto.append(CostoAux)
+
+    Tabla = pd.DataFrame({"NumeroImagen":ListaNumeroImagen,"TituloCabecera":ListaTituloCabecera,"Direccion":ListaDireccionCabecera,"Costo":ListaCosto})
+    return(Tabla)
 
 print(Peticion('https://www.inmuebles24.com/casas-en-venta.html'))
